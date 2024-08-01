67b6ef03090fd218d7d99e3f4a098ad3ebda0860
donghapark <donghaha12@naver.com>
2024-08-01 11:12:22 +0900

    메인 겹침

--- bank/main.cpp
+++ bank/main.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
+#include<vector>
+#include"Person.h"
+#include"User.h"
+#include"Admin.h"
+#include"Account.h"
 using namespace std;
 int main() {
+	Account temp = {1231124,100000};
 
 
+<<<<<<< Updated upstream
 	cout <<" 메인 변경";
+=======
+	cout << temp.get_account_num() << endl;
+	cout << temp.get_balance() << endl;
+
+
+
+	return 0;
+>>>>>>> Stashed changes
 }
