06c36a6f7768046e956c30b27ec482de50c59e5b
YOUNGHO0 <31pqopq13@gmail.com>
2024-08-02 00:04:24 +0900

    Feat : user.h 메서드 목록 구현

--- bank/User.h
+++ bank/User.h
@@ -12,6 +12,14 @@ public:
 	Account * add_account();
 	User (string id, string pw, string name);
 	~User ();
+	void deposit( long long account_num, long amount);
+	void withDraw(long long  account_num , long amount);
+	double getBalance( long long account_num);
+	Account* get_user_account(long long account_num);
 	long long generateAccountNumber();
+	void show_all_accounts();
+
+private:
+	Account* findAccountByNumber(vector<unique_ptr<Account>>& accounts, long long account_num);
 };
 
