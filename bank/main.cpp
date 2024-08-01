142448f851860957d511bcc74bad1f1ae6dc2d7e
YOUNGHO0 <31pqopq13@gmail.com>
2024-08-01 15:14:01 +0900

    Feat : Account 테스트로 main변경

--- bank/main.cpp
+++ bank/main.cpp
@@ -4,13 +4,16 @@
 #include"User.h"
 #include"Admin.h"
 #include"Account.h"
+#include "AccountManager.h"
 using namespace std;
 int main() {
-	Account temp = {1231124,100000};
 
+	AccountManager m;
+	User user("user","pass","userName");
 
-	cout << temp.get_account_num() << endl;
-	cout << temp.get_balance() << endl;
+	 m.create_account(user).get_account_num();
+
+	for(Account s : user.get_user_account()) cout << s.get_account_num() << endl;
 
 
 	return 0;
