2742efaa25f05a6ce50869498da0aaa0d2a9d9cd
YOUNGHO0 <31pqopq13@gmail.com>
2024-08-11 13:20:13 +0900

    feat : AccountLog 생성

---
+++ QTbank/AccountLog.cpp
@@ -0,0 +1,7 @@
+//
+// Created by lee on 2024. 8. 10..
+//
+
+#include "AccountLog.h"
+
+#include <QtCore>
---
+++ QTbank/AccountLog.h
@@ -0,0 +1,14 @@
+//
+// Created by lee on 2024. 8. 10..
+//
+
+#ifndef ACCOUNTLOG_H
+#define ACCOUNTLOG_H
+#include <string>
+
+
+class AccountLog {
+public:
+    virtual std::string show_log() = 0;
+};
+#endif //ACCOUNTLOG_H
