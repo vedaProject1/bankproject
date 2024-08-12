3481f58162fb913166b3b536d6ae6c66525589fa
YOUNGHO0 <31pqopq13@gmail.com>
2024-08-12 21:55:54 +0900

    Feat: AccountLogger 입출금 로그(사용자,전체) 반환 추가

--- QTbank/AccountLogger.cpp
+++ QTbank/AccountLogger.cpp
@@ -6,6 +6,8 @@
 
 #include <QtCore>
 
+#include "Admin.h"
+extern Admin admin;
 AccountLogger AccountLogger::logger;
 
 AccountLogger& AccountLogger::getLogger() {
@@ -19,6 +21,66 @@ void AccountLogger::set_log(std::shared_ptr<AccountLog> log) {
 int AccountLogger::get_size() {
     return logVector.size();
 }
+
+User * AccountLogger::find_by_id(string id) {
+
+   int idx= admin.search_user_return_idx(id);
+    if(idx == 1000) return nullptr;
+    User *user =dynamic_cast<User *>(admin.user_list[idx]);
+    return user;
+}
+
+std::vector<std::string> AccountLogger::get_all_logs() {
+    std::vector<std::string> result;
+    for(std::shared_ptr<AccountLog> log :logVector) {
+        result.push_back(log->show_log());
+    }
+    return result;
+}
+
+std::vector<std::string> AccountLogger::get_all_deposit_logs() {
+
+    vector<std::string> result;
+    for(shared_ptr<AccountLog> log : logVector) {
+        auto depositLog = std::dynamic_pointer_cast<AccountDepositLog>(log);
+        // If cast successful, add to result vector
+        if (depositLog) {
+            result.push_back(depositLog->show_log());
+        }
+    }
+    return result;
+
+
+
+}
+
+std::vector<std::string> AccountLogger::get_all_withdraw_logs() {
+
+    vector<std::string> result;
+    for(shared_ptr<AccountLog> log : logVector) {
+        auto depositLog = std::dynamic_pointer_cast<AccountWithdrawLog>(log);
+        // If cast successful, add to result vector
+        if (depositLog) {
+            result.push_back(depositLog->show_log());
+        }
+    }
+    return result;
+}
+
+std::vector<std::string> AccountLogger::get_all_user_logs(string id) {
+
+    User * foundUser = find_by_id(id);
+
+    vector<std::string> result;
+    if(foundUser ==nullptr) return result;
+    for(shared_ptr<AccountLog> log :logVector) {
+        if(log->get_loged_user() == foundUser) {
+            result.push_back(log->show_log());
+        }
+    }
+    return result;
+}
+
 
 void AccountLogger::show_all_log() {
     for(std::shared_ptr<AccountLog> log :logVector) {
--- QTbank/AccountLogger.h
+++ QTbank/AccountLogger.h
@@ -15,12 +15,17 @@ class AccountLogger {
 
     static AccountLogger logger;
     std::vector<std::shared_ptr<AccountLog>> logVector;
+    User *find_by_id(string id);
 public:
     AccountLogger(){};
     static AccountLogger& getLogger();
     void set_log(std::shared_ptr<AccountLog> log);
     void show_all_log();
     int get_size();
+    std::vector<std::string> get_all_logs();
+    std::vector<std::string> get_all_deposit_logs();
+    std::vector<std::string> get_all_withdraw_logs();
+    std::vector<std::string> get_all_user_logs(string id);
 };
 
 #endif //ACCOUNTLOGGER_H
