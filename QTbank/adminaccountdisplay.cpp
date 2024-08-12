6a8236c5f9d5269c347c94613cf39311ac3f5019
YOUNGHO0 <31pqopq13@gmail.com>
2024-08-12 23:39:10 +0900

    Feat: 사용자 검색 시 모든 계좌정보를 가져오게 변경

--- QTbank/adminaccountdisplay.cpp
+++ QTbank/adminaccountdisplay.cpp
@@ -8,8 +8,10 @@
 
 #include "Account.h"
 #include "AccountLogger.h"
+#include "Admin.h"
 #include "ui_AdminAccountDisplay.h"
 
+extern Admin admin;
 
 AdminAccountDisplay::AdminAccountDisplay(QWidget *parent) :
     QWidget(parent), ui(new Ui::AdminAccountDisplay) {
@@ -67,11 +69,33 @@ void AdminAccountDisplay::show_all_deposit_logs() {
 void AdminAccountDisplay::show_user_transations() {
     qDebug() << "button clicked user transaction";
     AccountLogger &logger = AccountLogger::getLogger();
-    std::vector<std::string> result = logger.get_all_user_logs(ui->idSearch->text().toStdString());
+
+    int idx = admin.search_user_return_idx(ui->idSearch->text().toStdString());
+    if(idx == 1000) return;
+
+    ui->userName->setText(QString::fromStdString(admin.user_list[idx]->get_name()));
+    ui->userBalance->setText(QString::number(admin.user_list[idx]->get_user_account().get_balance()));
+    // 사용자 계좌 설정
+    vector<shared_ptr<Account>> accountList = dynamic_cast<User* >(admin.user_list[idx])->get_all_accounts();
     QStringList initialData;
-    if(result.size()==0 )return;
-    for(string res : result) initialData.push_back(QString::fromStdString(res));
+
+    for(int i =0; i<accountList.size(); i++) {
+
+        initialData.push_back(QString::number(accountList.at(i)->get_account_num()));
+
+    }
     QStringListModel* model = new QStringListModel(this);
     model->setStringList(initialData);
-    ui->userAccountTransaction->setModel(model);
+    ui->userAccountList->setModel(model);
+
+    //유저 로그를 셋팅해서 보여줌
+    User *user = dynamic_cast<User*>(admin.user_list[idx]);
+
+    std::vector<std::string> result = logger.get_all_user_logs(ui->idSearch->text().toStdString());
+    QStringList userTransactionalData;
+    if(result.size()==0 )return;
+    for(string res : result) userTransactionalData.push_back(QString::fromStdString(res));
+    QStringListModel* userAccountTransactionModel = new QStringListModel(this);
+    userAccountTransactionModel->setStringList(userTransactionalData);
+    ui->userAccountTransaction->setModel(userAccountTransactionModel);
 }
