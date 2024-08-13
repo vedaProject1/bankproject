b0d9fd8c7516b001ff3df487d8324bf3d1b977b0
YOUNGHO0 <31pqopq13@gmail.com>
2024-08-13 18:07:56 +0900

    Remove : 필요없는 주석 삭제 및

--- QTbank/useraccountdisplay.cpp
+++ QTbank/useraccountdisplay.cpp
@@ -20,28 +20,6 @@ UserAccountDisplay::UserAccountDisplay(QWidget *parent) : QWidget(parent),ui(new
 void UserAccountDisplay::set_user(User& user){
     // this->user = &user;
 
-    // // 기존 UI 초기화
-    // ui->userName->clear();
-    // ui->userBalance->clear();
-    // ui->userAccountNumber->clear();
-    // ui->listView_2->setModel(nullptr);
-
-    // // 새 사용자 정보로 UI 업데이트
-    // ui->userName->setText(QString::fromStdString(user.get_name()));
-    // ui->userBalance->setText(QString::number(user.get_user_account().get_balance()));
-    // ui->userAccountNumber->setText(QString::number(user.get_user_account().get_account_num()));
-    // ui->listView_2->setEditTriggers(QAbstractItemView::NoEditTriggers);
-    // vector<shared_ptr<Account>> accountList = user.get_all_accounts();
-    // QStringList initialData;
-
-    // for (int i = 0; i < accountList.size(); i++) {
-    //     initialData.push_back(QString::number(accountList.at(i)->get_account_num()));
-    // }
-    // model = new QStringListModel(this);
-    // model->setStringList(initialData);
-
-    // ui->listView_2->setModel(model);
-
     this->user = &user;
     ui->userName->setText(QString::fromStdString(user.get_name()));
     ui->userBalance->setText(QString::number(user.get_user_account().get_balance()));
